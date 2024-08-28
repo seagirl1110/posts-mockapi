144e8bed4106aca0bac1fe440def0573e801aac1
Marina Elizarova <seagirl1110@gmail.com>
2024-08-28 12:55:37 +0300

    feat: add header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,5 +1,21 @@
 import styles from './styles.module.css';
+import Logo from './../../assets/icons/logo-white.svg';
+
+const navItems = ['Главная', 'Музыка', 'Сообщества', 'Друзья'];
 
 export default function Header() {
-  return <div></div>;
+  return (
+    <header className={styles.header}>
+      <div className={styles.header_container}>
+        <img className={styles.logo} src={Logo} alt="Logo_MECK" />
+        <nav className={styles.nav_list}>
+          {navItems.map((item, index) => (
+            <div className={styles.nav_item} key={index}>
+              {item}
+            </div>
+          ))}
+        </nav>
+      </div>
+    </header>
+  );
 }
